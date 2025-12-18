993e34659dbf88c48858d7c70fd176908e6172dc
BrandyAllan <brandyratsimbaharison46@gmail.com>
2025-12-18 08:14:24 +0300

    change title

--- app/views/layout.php
+++ app/views/layout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Taxi Moto – Gestion</title>
+    <title>SpeedMoto</title>
 
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/typography.css">
